4eaffd235b7cc4c8d05dcb9714ef6d7fd81e32e9
frank-1992 <103110407+frank-1992@users.noreply.github.com>
2022-04-14 17:19:29 +0800

    Initial commit

---
+++ Example/WYARModule/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WYARModule
+//
+//  Created by frank-1992 on 04/15/2022.
+//  Copyright (c) 2022 frank-1992. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
